66425d461efd366faa7b8ef5b7476c1fbf0e1b53
WasawatB <bun.wasawat@outlook.com>
2023-08-05 23:21:35 +0100

    edit comment

--- code/infrastructure/main.tf
+++ code/infrastructure/main.tf
@@ -38,40 +38,8 @@ module "db_instance" {
 
 }
 
-
-# image registry
-# module "ecr_image" {
-#    source = "./modules/ecr"
-#    ecr_repo_name = "${var.ecr_repo_name}_${var.project_id}"
-#    account_id = local.account_id
-#    lambda_function_local_path = var.lambda_function_local_path
-#    docker_image_local_path = var.docker_image_local_path
-# }
-
-# module "lambda_function" {
-#   source = "./modules/lambda"
-#   image_uri = module.ecr_image.image_uri
-#   lambda_function_name = "${var.lambda_function_name}_${var.project_id}"
-#   model_bucket = module.s3_bucket.name
-#   output_stream_name = "${var.output_stream_name}-${var.project_id}"
-#   output_stream_arn = module.output_kinesis_stream.stream_arn
-#   source_stream_name = "${var.source_stream_name}-${var.project_id}"
-#   source_stream_arn = module.source_kinesis_stream.stream_arn
-# }
-
 # For CI/CD
-# output "lambda_function" {
-#   value     = "${var.lambda_function_name}_${var.project_id}"
-# }
+output "model_bucket" {
+  value = module.s3_bucket.name
+}
 
-# output "model_bucket" {
-#   value = module.s3_bucket.name
-# }
-
-# output "predictions_stream_name" {
-#   value     = "${var.output_stream_name}-${var.project_id}"
-# }
-
-# output "ecr_repo" {
-#   value = "${var.ecr_repo_name}_${var.project_id}"
-# }
